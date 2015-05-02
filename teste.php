e23ddb6ae18706360d00a28a65bd0bcc33ae7416
Rogger Alexander <rogger.alexander@ibridge.com.br>
2015-05-01 21:09:30 -0300

    Apagando a invocação da função para obter as informações do servidor apache

--- teste.php
+++ teste.php
@@ -1,4 +1,17 @@
 <?php
- echo php_version();
- echo phpinfo();
+ // GERANDO UM NOVO ARRAY 
+ $objArray = [];
+ $objArray.push(5);
+ $objArray.push(10);
+ $objArray.push(15);
+
+ // PERCORRENDO O ARRAY CRIADO
+ print "O tamanho do array criado é " . count($objArray) . "<br>";
+ print "Os elementos do array são: <br>";
+ foreach($objArray as $elm => $value) {
+   print "Elemento $elm: $value<br>";
+ }
+
+ // LIBERANDO O OBJETO ARRAY PARA A COLETA DE LIXO
+ unset($objArray);
 ?>
